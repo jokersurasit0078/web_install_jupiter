56897043be72bc6ca8ecffab6dca673ee89d2024
Surasit Suwannara <zsurasit.s@pttdigital.com>
2023-09-22 12:16:50 +0700

    fix image logo jupiter

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ const Index = () => {
   const link = ['itms-services://', '?action=download-manifest', `&url=${process.env.NEXT_PUBLIC_HOSTNAME}manifest.plist`];
   const ios = link.join('')
   return (
-    <div className="bg-twilight-blue ">
+    <div className="bg-twilight-blue flex justify-center items-center h-screen">
       <div className="max-w-md m-auto">
-        <img src="proof-of-delivery.png" className="max-w-xs m-auto" />
+        <img src="jupiter_top.png" className="max-w-xs m-auto" draggable={false}/>
         <img src="logo.png" className="my-8 px-4 max-w-xs m-auto" />
         <div className="flex flex-col justify-center items-center">
-          <a href={ios} className="flex items-center mb-4">
-            <img src="ios.png" className="w-8 mr-2" />
-            Install On iOS Version {process.env.NEXT_PUBLIC_IOS_VERSION} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a href={ios} className="flex items-center mb-4 mt-4 lg:text-lg text-[20px]">
+            <img src="ios.png" className="w-8 mr-2"/>
+            Install on iOS Version {process.env.NEXT_PUBLIC_IOS_VERSION}
           </a>
-          <a href="app-release.apk" className="flex items-center">
+          <a href="app-release.apk" className="flex items-center lg:text-lg text-[20px]">
             <img src="android.png" className="w-8 mr-2" />
-            <a href="app-release.apk">Install On Android Version {process.env.NEXT_PUBLIC_ANDROID_VERSION} </a>
+            <a href="app-release.apk">Install on Android Version {process.env.NEXT_PUBLIC_ANDROID_VERSION} </a>
           </a>
-          <div className="mt-8 text-xs">PTT Digital Solution Co., Ltd.</div>
+          <div className="mt-8 text-sm">PTT Digital Solution Co., Ltd.</div>
         </div>
       </div>
     </div>
