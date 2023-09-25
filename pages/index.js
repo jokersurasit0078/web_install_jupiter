40900f954838a2ca01d9e207bdc1826df0fc1b93
Surasit Suwannara <zsurasit.s@pttdigital.com>
2023-09-25 11:56:42 +0700

    fix issue coupon & text message permission camera

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ const Index = () => {
     "?action=download-manifest",
     `&url=${process.env.NEXT_PUBLIC_HOSTNAME}manifest.plist`,
   ];
+  const dateUpdateLasted = '25/09/2023 13:00';
   const ios = link.join("");
   return (
     <div className="bg-twilight-blue flex justify-center items-center h-screen p-4">
@@ -17,7 +18,7 @@ const Index = () => {
         <div className="flex flex-col justify-center items-center">
           <a
             href={ios}
-            className="flex items-center mb-4 mt-4 lg:text-lg text-[16px]"
+            className="flex items-center mb-4 mt-4 lg:text-lg text-[16px] font-bold"
           >
             <img src="ios.png" className="w-8 mr-2" />
             Install Jupiter(PTTPLC) on iOS Version{" "}
@@ -25,13 +26,16 @@ const Index = () => {
           </a>
           <a
             href="jupiter.apk"
-            className="flex items-center lg:text-lg text-[16px]"
+            className="flex items-center lg:text-lg text-[16px] font-bold"
           >
             <img src="android.png" className="w-8 mr-2" />
             Install Jupiter(PTTPLC) on Android Version{" "}
             {process.env.NEXT_PUBLIC_PTTPLC_ANDROID_VERSION}
           </a>
-          <div className="mt-8 text-sm">PTT Digital Solution Co., Ltd.</div>
+          <div className="mt-4 text-[16px] font-bold">
+            {'Last updated on : ' + dateUpdateLasted}
+          </div>
+          <div className="mt-6 text-sm">PTT Digital Solution Co., Ltd.</div>
         </div>
       </div>
     </div>
