88775556e75f19877cff134a8ad6a9e235898b0e
Surasit Suwannara <z.surasit@pttdigital.com>
2023-11-03 19:15:03 +0700

    update version prod 1.0.0 to 1.0.1

--- pages/android.js
+++ pages/android.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import RefectProvider from "../provider/RefetchProvider"
 
 const Index = () => {
-  const jupiterApk = 'jupiter-09102023-2.apk';
-  const dateUpdateLasted = '09/10/2023 17:50 (1)';
+  const jupiterApk = 'jupiter-03112023.apk';
+  const dateUpdateLasted = '03/11/2023 19:00';
   return (
     <div className='bg-twilight-blue flex justify-center items-center h-screen p-4'>
-      <title>Install PTTPLC</title>
-      <div className='max-w-md m-auto'>
-        <div className='max-w-xs m-auto'>
-          <img src='jupiter_top.png' draggable={false} />
-        </div>
-        <img src='logo.png' className='my-8 px-4 max-w-xs m-auto' />
-        <div className='flex flex-col justify-center items-center'>
-          <div className='bg-lime-200 px-4 rounded-xl'>
-            <a
-              href={jupiterApk}
-              className='flex items-center mb-4 mt-4 lg:text-lg text-[16px] font-bold text-black'
-            >
-              <img src='android.png' className='w-8 mr-2' />
-              Install Jupiter(PTTPLC) for Android
-            </a>
+      <title>Update PTTPLC</title>
+      <RefectProvider>
+        <div className='max-w-md m-auto'>
+          <div className='max-w-xs m-auto'>
+            <img src='jupiter_top.png' draggable={false} />
           </div>
-          <div className='mt-4 text-[16px] font-bold'>
-            {'Last updated on : ' + dateUpdateLasted}
+          <img src='logo.png' className='my-8 px-4 max-w-xs m-auto' />
+          <div className='flex flex-col justify-center items-center'>
+            <div className='bg-lime-200 px-4 rounded-xl'>
+              <a
+                href={jupiterApk}
+                className='flex items-center mb-4 mt-4 lg:text-lg text-[16px] font-bold text-black'
+              >
+                <img src='android.png' className='w-8 mr-2' />
+                Update Jupiter(PTTPLC) for Android v{process.env.NEXT_PUBLIC_PTTPLC_ANDROID_VERSION}
+              </a>
+            </div>
+            <div className='mt-4 text-[16px] font-bold'>
+              {'Last updated on : ' + dateUpdateLasted}
+            </div>
+            <div className='mt-6 text-sm'>PTT Digital Solution Co., Ltd.</div>
           </div>
-          <div className='mt-6 text-sm'>PTT Digital Solution Co., Ltd.</div>
         </div>
-      </div>
+      </RefectProvider>
     </div>
   );
 };
--- pages/ios.js
+++ pages/ios.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RefectProvider from "../provider/RefetchProvider"
 
 const Index = () => {
   const link = [
@@ -6,32 +7,34 @@ const Index = () => {
     '?action=download-manifest',
     `&url=${process.env.NEXT_PUBLIC_HOSTNAME}manifest.plist`,
   ];
-  const dateUpdateLasted = '09/10/2023 17:50 (1)';
+  const dateUpdateLasted = '03/11/2023 19:00';
   const ios = link.join('');
   return (
     <div className='bg-twilight-blue flex justify-center items-center h-screen p-4'>
-      <title>Install PTTPLC</title>
-      <div className='max-w-md m-auto'>
-        <div className='max-w-xs m-auto'>
-          <img src='jupiter_top.png' draggable={false} />
-        </div>
-        <img src='logo.png' className='my-8 px-4 max-w-xs m-auto' />
-        <div className='flex flex-col justify-center items-center'>
-          <div className='bg-blue-400 px-4 rounded-xl'>
-            <a
-              href={ios}
-              className='flex items-center mb-4 mt-4 lg:text-lg text-[16px] font-bold text-white'
-            >
-              <img src='ios.png' className='w-8 mr-2' />
-              Install Jupiter(PTTPLC) for iOS
-            </a>
+      <title>Update PTTPLC</title>
+      <RefectProvider>
+        <div className='max-w-md m-auto'>
+          <div className='max-w-xs m-auto'>
+            <img src='jupiter_top.png' draggable={false} />
           </div>
-          <div className='mt-4 text-[16px] font-bold'>
-            {'Last updated on : ' + dateUpdateLasted}
+          <img src='logo.png' className='my-8 px-4 max-w-xs m-auto' />
+          <div className='flex flex-col justify-center items-center'>
+            <div className='bg-blue-400 px-4 rounded-xl'>
+              <a
+                href={ios}
+                className='flex items-center mb-4 mt-4 lg:text-lg text-[16px] font-bold text-white'
+              >
+                <img src='ios.png' className='w-8 mr-2' />
+                Update Jupiter(PTTPLC) for iOS v{process.env.NEXT_PUBLIC_PTTPLC_IOS_VERSION}
+              </a>
+            </div>
+            <div className='mt-4 text-[16px] font-bold'>
+              {'Last updated on : ' + dateUpdateLasted}
+            </div>
+            <div className='mt-6 text-sm'>PTT Digital Solution Co., Ltd.</div>
           </div>
-          <div className='mt-6 text-sm'>PTT Digital Solution Co., Ltd.</div>
         </div>
-      </div>
+      </RefectProvider>
     </div>
   );
 };
