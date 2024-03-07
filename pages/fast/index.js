12690baa3fdcd845fbc861266743dbae853812a2
Surasit Suwannara <zsurasit.s@pttdigital.com>
2024-03-07 10:50:16 +0700

    update fast

--- pages/fast/index.js
+++ pages/fast/index.js
@@ -8,7 +8,7 @@ const Index = () => {
     `&url=${process.env.NEXT_PUBLIC_FAST_HOSTNAME}/manifest-fast.plist`,
   ];
   const jupiterApk = '/fast/app-release-fast.apk';
-  const dateUpdateLasted = '06/03/2024 09:50 (2)';
+  const dateUpdateLasted = '07/03/2024 10:50 (1)';
   const ios = link.join('');
   const prepath = '../';
   const description = 'DESCRIPTION CER:PLC|ORG:JU0001|BRANCH:PRE-MERGE-RFID-ROUTE';
