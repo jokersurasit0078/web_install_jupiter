a356e684ce6ddbbc56697a8b919386ad922f0af1
Surasit Suwannara <zsurasit.s@pttdigital.com>
2024-03-15 10:52:45 +0700

    update mea

--- pages/mea/index.js
+++ pages/mea/index.js
@@ -8,8 +8,8 @@ const Index = () => {
     `&url=${process.env.NEXT_PUBLIC_MEA_HOSTNAME}/manifest-mea-dev.plist`,
   ];
   const jupiterApk = 'jupiter-mea.apk';
-  const dateUpdateLastediOS = '11/03/2024 17:00 (1)';
-  const dateUpdateLastedAndroid = '11/03/2024 17:00 (1)';
+  const dateUpdateLastediOS = '15/03/2024 10:55 (1)';
+  const dateUpdateLastedAndroid = '15/03/2024 10:55 (1)';
   const ios = link.join('');
   const prepath = '../';
 
